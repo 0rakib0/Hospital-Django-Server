19bf6ffa7e306ad86cbab0790943385b22d449c3
Rakibul Hasan <hassanrakibul926@gmail.com.com>
2024-02-21 01:54:51 +0600

    create add patients models and register in admin file

--- Patients/models.py
+++ Patients/models.py
@@ -2,5 +2,19 @@ from django.db import models
 
 # Create your models here.
 
+class AddPatients(models.Model):
+    full_name = models.CharField(max_length=160)
+    date_of_birth = models.DateField()
+    age = models.CharField(max_length=20)
+    phone = models.CharField(max_length=20)
+    email = models.EmailField()
+    gender = models.CharField(max_length=20)
+    full_address = models.CharField(max_length=260)
+    details = models.TextField()
+    patients_pic = models.ImageField(upload_to='patients')
+    create_at = models.DateTimeField(auto_now_add=True)
 
+
+    def __str__(self) -> str:
+        return self.full_name
 
