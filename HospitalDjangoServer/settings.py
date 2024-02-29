d934ec00642cf9894caa9d3013201b4df8ca0bed
Rakibul Hasan <hassanrakibul926@gmail.com>
2024-03-01 03:36:33 +0600

    Configar SMTP for send email

--- HospitalDjangoServer/settings.py
+++ HospitalDjangoServer/settings.py
@@ -144,3 +144,14 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+# SMTP Configaration
+
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_PORT=587
+EMAIL_HOST_USER='my_email'
+EMAIL_HOST_PASSWORD='email_password'
+EMAIL_USE_TLS=True
+
