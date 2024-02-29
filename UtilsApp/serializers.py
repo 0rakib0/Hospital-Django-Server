5e9ef300cf18ca6c38da50cd1e2b7fe7becc81e2
Rakibul Hasan <hassanrakibul926@gmail.com>
2024-02-29 08:32:57 +0600

    create a serializer for appoinment

--- UtilsApp/serializers.py
+++ UtilsApp/serializers.py
@@ -1,8 +1,13 @@
 from rest_framework import serializers
-from .models import Payment
+from .models import Payment, Appoinments
 
 
 class PaymentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Payment
+        fields = '__all__'
+
+class AppoinmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Appoinments
         fields = '__all__'
