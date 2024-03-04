df8a8a00a078775822954131f38f2e19cc257eaa
Rakibul Hasan <hassanrakibul926@gmail.com>
2024-03-04 22:21:48 +0600

    createsuperuser

--- Accounts/models.py
+++ Accounts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class CustomUser(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True)
+    user_roll = (
+        ('admin','admin'),
+        ('doctor','doctor'),
+        ('patients','patients'),
+    )
+
+    user_type = models.CharField(max_length=20, choices=user_roll, default = 'admin')
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['email']
