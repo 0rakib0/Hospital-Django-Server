e1f2aa6a30d07b0329c907e61c36669fe1560689
rakib <hassanrakibul926@gmail.com>
2024-03-07 12:08:28 +0600

    add token custom cliems

--- Accounts/urls.py
+++ Accounts/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path
+from . import views
+from rest_framework_simplejwt.views import TokenRefreshView
 
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
 
 urlpatterns = [
-    path('auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('auth/token/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
