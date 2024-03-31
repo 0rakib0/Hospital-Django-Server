3fe18ac45008150d9b38d4a1cc5656cd9ff99252
Rakibul Hasan <hassanrakibul926@gmail.com>
2024-04-01 01:58:36 +0600

    change email title

--- UtilsApp/sendMail.py
+++ UtilsApp/sendMail.py
@@ -3,7 +3,7 @@ from django.core.mail import send_mail
 
 def pattiemtsAccountCreateMail(email, password):
     send_mail(
-        "Gulshan Clinick",
+        "Health Care",
         f"Hello! Your Patients sucessfully admited. please login our website  with our provided cradential and see update about patients Password: {password} and Email: {email}",
         "hassanrakibul926@gmail.com",
         [email],
@@ -12,7 +12,7 @@ def pattiemtsAccountCreateMail(email, password):
 
 def paymentConfirmMail(email, PatientsId, service, paymentType, costOfTreatment):
     send_mail(
-        "Gulshan Clinick",
+        "Health Care",
         f"Your Payment Successfully Paid,  Patient Id: {PatientsId},  Service Name: {service},  Payment Type: {paymentType},  Total Amount: {costOfTreatment}tk",
         "hassanrakibul926@gmail.com",
         [email],
@@ -22,7 +22,7 @@ def paymentConfirmMail(email, PatientsId, service, paymentType, costOfTreatment)
 
 def DoctorAccountCreateMail(email, password):
     send_mail(
-        "Gulshan Clinick",
+        "Health Care",
         f"Hello! Doctor. Your account successfully ceated please go heath care website and login by using email and password and get update abour work. Password: {password} and Email: {email}",
         "hassanrakibul926@gmail.com",
         [email],
